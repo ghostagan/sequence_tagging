c186d1251e35fc89d88c6328b1aaed2fd565fbfc
Chenglong Ma <machenglong@pachiratech.com>
2018-12-28 16:21:11 +0800

    running conll2003 dataset,f1 of test=90.26

--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 glove:
 	wget -P ./data/ "http://nlp.stanford.edu/data/glove.6B.zip"
 	unzip ./data/glove.6B.zip -d data/glove.6B/
-	rm ./data/glove.6B.zip
+#	rm ./data/glove.6B.zip
 
 run:
-	python build_data.py
-	python train.py
-	python evaluate.py
+	CUDA_VISIBLE_DEVICES=2 python build_data.py
+	CUDA_VISIBLE_DEVICES=2 python train.py
+	CUDA_VISIBLE_DEVICES=2 python evaluate.py
--- model/config.py
+++ model/config.py
@@ -71,11 +71,11 @@ class Config():
     use_pretrained = True
 
     # dataset
-    # filename_dev = "data/coNLL/eng/eng.testa.iob"
-    # filename_test = "data/coNLL/eng/eng.testb.iob"
-    # filename_train = "data/coNLL/eng/eng.train.iob"
+    filename_dev = "data/coll2003_valid_boi.txt"
+    filename_test = "data/coll2003_test_boi.txt"
+    filename_train = "data/coll2003_train_boi.txt"
 
-    filename_dev = filename_test = filename_train = "data/test.txt" # test
+    # filename_dev = filename_test = filename_train = "data/test.txt" # test
 
     max_iter = None # if not None, max number of examples in Dataset
 
